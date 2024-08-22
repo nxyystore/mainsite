33412040ed20c0f45c0a9f1708c1a28dca8f89d7
Nxyy <nxyy@nxyy.store>
2024-08-22 14:14:01 -0400

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
 
 AOS.init();
 
@@ -168,3 +179,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 }
+/*
+     FILE ARCHIVED ON 07:21:55 Jan 04, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:57:51 Aug 22, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.711
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.012
+  esindex: 0.019
+  cdx.remote: 1254.864
+  LoadShardBlock: 1661.0 (3)
+  PetaboxLoader3.resolve: 3228.932 (3)
+  PetaboxLoader3.datanode: 213.76 (4)
+  load_resource: 2454.914
+*/
